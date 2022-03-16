411e7136c135b74b377adf852fe5a9ab621b822a
wan-nyan-wan <distributed.system.love@gmail.com>
2022-03-16 15:29:00 +0900

    Add template

---
+++ template.rs
@@ -0,0 +1,12 @@
+use proconio::input;
+use proconio::marker::Chars;
+use std::cmp;
+use std::collections::HashMap;
+
+fn main() {
+    input! {
+        n: usize,
+        xys: [(i32, i32);n],
+        s: Chars,
+    }
+}
