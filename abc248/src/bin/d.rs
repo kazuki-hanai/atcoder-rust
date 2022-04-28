b66c90750cd833e3eba1ee16a8023dee4136ca00
wan-nyan-wan <distributed.system.love@gmail.com>
2022-04-29 01:10:54 +0900

    Add abc248 d

--- abc248/src/bin/d.rs
+++ abc248/src/bin/d.rs
@@ -1,3 +1,29 @@
+use proconio::{input, marker::Usize1};
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        mut va: [usize; n],
+        q_num: usize
+    }
+
+    let mut a_pos: Vec<Vec<usize>> = vec![vec![]; n + 1];
+
+    for (i, a) in va.iter().enumerate() {
+        a_pos[*a - 1].push(i);
+    }
+
+    for _ in 0..q_num {
+        input! {
+            l: Usize1,
+            r: Usize1,
+            x: Usize1,
+        }
+
+        let lower_pos = a_pos[x].partition_point(|&v| v < l);
+        let upper_pos = a_pos[x].partition_point(|&v| v <= r);
+
+        let ret = upper_pos - lower_pos;
+        println!("{}", ret);
+    }
 }
