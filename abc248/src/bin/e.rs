50d1de6f6eacf3b6764be1191de404573d56a110
wan-nyan-wan <distributed.system.love@gmail.com>
2022-04-29 14:34:54 +0900

    Add abc248 e

--- abc248/src/bin/e.rs
+++ abc248/src/bin/e.rs
@@ -1,3 +1,53 @@
+extern crate proconio;
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        k: usize,
+        xys: [(i64, i64); n],
+    }
+
+    if k == 1 {
+        println!("Infinity");
+        return;
+    }
+
+    fn is_on(xy1: (i64, i64), xy2: (i64, i64), xy3: (i64, i64)) -> bool {
+        (xy1.0 - xy2.0) * (xy1.1 - xy3.1) == (xy1.0 - xy3.0) * (xy1.1 - xy2.1) 
+    }
+
+    let mut is_checked = vec![vec![false; n]; n];
+
+    let mut ans = 0;
+    for i1 in 0..n {
+        for i2 in i1 + 1..n {
+            if is_checked[i1][i2] {
+                continue;
+            }
+
+            let mut overlap_points = vec![i1, i2];
+
+            let mut cnt = 2;
+            for i3 in i2 + 1..n {
+                if is_on(xys[i1], xys[i2], xys[i3]) {
+                    overlap_points.push(i3);
+                    cnt += 1;
+                }
+            }
+
+            for i in &overlap_points {
+                for j in &overlap_points {
+                    is_checked[*i as usize][*j as usize] = true;
+                }
+            }
+
+            if k <= cnt {
+                ans += 1;
+            }
+        }
+    }
+
+
+    println!("{}", ans);
 }
