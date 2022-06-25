ac1059ca8638bba773d4a218f5c923173d245779
wan-nyan-wan <distributed.system.love@gmail.com>
2022-06-25 17:20:06 +0900

    Add abc254 c

--- abc254/src/bin/c.rs
+++ abc254/src/bin/c.rs
@@ -1,9 +1,46 @@
 use proconio::input;
 
+#[allow(non_snake_case)]
 fn main() {
     input! {
         N: usize,
         K: usize,
         A: [u64; N]
+    }
+
+    if K == 1 {
+        println!("Yes");
+        return;
+    }
+
+    let mut array_by_k: Vec<Vec<u64>> = vec![vec! {}; K];
+    for i in 0..N {
+        array_by_k[i % K].push(A[i]);
+    }
+
+    for i in 0..K {
+        array_by_k[i].sort_by(|a, b| b.cmp(a));
+    }
+
+    let mut sorted = true;
+    let mut bef = 0;
+    for i in 0..N {
+        if let Some(val) = array_by_k[i % K].pop() {
+            if bef <= val {
+                bef = val;
+            } else {
+                sorted = false;
+                break;
+            }
+        } else {
+            sorted = false;
+            break;
+        }
+    }
+
+    if sorted {
+        println!("Yes");
+    } else {
+        println!("No");
     }
 }
