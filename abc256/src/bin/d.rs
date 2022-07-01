1249be33cac6320ea4cbc81f9044d7a51f501bf1
wan-nyan-wan <distributed.system.love@gmail.com>
2022-07-01 15:32:13 +0900

    Add abc256 d

--- abc256/src/bin/d.rs
+++ abc256/src/bin/d.rs
@@ -1,3 +1,31 @@
+use proconio::input;
+
+#[allow(non_snake_case)]
 fn main() {
-    unimplemented!();
+    input! {
+        N: usize,
+        LR: [(i32, i32); N]
+    }
+
+    let mut logs = vec![];
+    for (l, r) in LR {
+        logs.push((l, 0));
+        logs.push((r, 1));
+    }
+    logs.sort();
+
+    let mut login_num = 0;
+    for (t, state) in logs {
+        if state == 0 {
+            if login_num == 0 {
+                print!("{} ", t);
+            }
+            login_num += 1;
+        } else {
+            if login_num == 1 {
+                println!("{}", t);
+            }
+            login_num -= 1;
+        }
+    }
 }
